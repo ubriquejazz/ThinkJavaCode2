62c750aaaf1f5848956459528ddee2e04da489e9
Marcelo Suisse <ubriquejazz@gmail.com>
2023-11-07 18:11:27 +0100

    eclipse files

--- ch03/GuessStarter.java
+++ ch03/GuessStarter.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  * Starter code for the "Guess My Number" exercise.
@@ -8,8 +9,18 @@ public class GuessStarter {
     public static void main(String[] args) {
         // pick a random number
         Random random = new Random();
-        int number = random.nextInt(100) + 1;
-        System.out.println(number);
+        Scanner in = new Scanner(System.in);
+        int desired = random.nextInt(100) + 1;
+        for (int i=0; i<3; i++){
+            System.out.print("Type your guess (" + i + ")"+ ": ");
+            int guessed = in.nextInt();
+            System.out.println("Your guess is: " + guessed);
+            System.out.println("I was thinking of: " + desired);
+            if (desired > guessed)
+                System.out.println("You were off by:" + (desired - guessed));
+            else 
+                System.out.println("You were off by:" + (guessed - desired));
+        }
     }
 
 }
--- ch05/Ex01.java
+++ ch05/Ex01.java
@@ -1,4 +1,13 @@
 public class Ex01 {
     public static void main(String[] args) {
+
+        boolean yes = true;
+        boolean no = false;
+        if ( no && !no || yes && !yes ) {
+            System.out.println("True");
+        }
+        else {
+            System.out.println("False");
+        }
     }
 }
